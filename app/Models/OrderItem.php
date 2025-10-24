37f0b98c9171ad3f1f8cde2d2a01e2dfc626abd5
Bkhangg <tkame1084@gmail.com>
2025-10-24 11:59:30 +0700

    Cập nhật model mới (thêm/ sửa các model)

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'quantity',
+        'price',
+    ];
+
+    public function products () {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function orders () {
+        return $this->belongsTo(Order::class);
+    }
+
+}
