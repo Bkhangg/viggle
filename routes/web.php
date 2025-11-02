b80ab0038614b31d087a71397889c8a6a8b8b1ee
Bkhangg <tkame1084@gmail.com>
2025-11-02 20:09:24 +0700

    Custom Middleware Kiểm Tra Người Dùng Đăng Nhập

--- routes/web.php
+++ routes/web.php
@@ -25,25 +25,29 @@ Route::get('/faq', function () {
     return view('clients.pages.faq');
 });
 
-// Register
-Route::get('/register', [AuthController::class,'showRegisterForm'])->name('register');
-Route::post('/register', [AuthController::class,'register'])->name('post-register');
+Route::middleware('guest')->group(function() {
+    // Register
+    Route::get('/register', [AuthController::class,'showRegisterForm'])->name('register');
+    Route::post('/register', [AuthController::class,'register'])->name('post-register');
+
+    // Log in
+    Route::get('/login', [AuthController::class,'showloginForm'])->name('login');
+    Route::post('/login', [AuthController::class,'login'])->name('post-login');
+
+    // Forgot password
+    Route::get('/forgot-password', [ForgotPasswordController::class,'showForgotPasswordForm'])->name('password.request');
+    Route::post('/forgot-password', [ForgotPasswordController::class,'sendResetLink'])->name('password.email');
+
+    // Forgot password
+    Route::get('/reset-password/{token}', [ResetPasswordController::class,'showResetForm'])->name('password.reset');
+    Route::post('/reset-password', [ResetPasswordController::class,'resetPassword'])->name('password.update');
+});
 
 // Activation Register
 Route::get('/activate/{token}', [AuthController::class,'activate'])->name('activate');
 
-// Log in
-Route::get('/login', [AuthController::class,'showloginForm'])->name('login');
-Route::post('/login', [AuthController::class,'login'])->name('post-login');
 
-// Logout
-Route::get('/logout', [AuthController::class,'logout'])->name('post-logout');
-
-// Forgot password
-Route::get('/forgot-password', [ForgotPasswordController::class,'showForgotPasswordForm'])->name('password.request');
-Route::post('/forgot-password', [ForgotPasswordController::class,'sendResetLink'])->name('password.email');
-
-// Forgot password
-Route::get('/reset-password/{token}', [ResetPasswordController::class,'showResetForm'])->name('password.reset');
-Route::post('/reset-password', [ResetPasswordController::class,'resetPassword'])->name('password.update');
-
+Route::middleware(['auth.custom'])->group(function() {
+    // Logout
+    Route::get('/logout', [AuthController::class,'logout'])->name('logout');
+});
